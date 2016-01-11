c544426ae9cb426cc0441781d551fe3dbb4ae3d2
Alex Aragon <Alex.Aragon@kingcounty.gov>
2016-01-11 08:17:44 -0800

    cleaning up syntax

--- tweet-intent.js
+++ tweet-intent.js
@@ -1,25 +1,25 @@
 
+(function() {
+  'use strict';
+  $.getScript('//platform.twitter.com/widgets.js', function()
+  {
 
-$.getScript('//platform.twitter.com/widgets.js', function()
-{
+    var twitterUrl = 'https://twitter.com/intent/tweet';
 
-  var twitterUrl = "https://twitter.com/intent/tweet";
+    var $tweetDivs = $('.tweet-intent');
 
-  var $tweetDivs = $('.tweet-intent');
+    $tweetDivs.each(function(){
 
-  $tweetDivs.each(function(){
+      var text = $(this).text().trim();
 
-       var text = $(this).text().trim();
+      var template = '<div class="tweet-intent"><div class="media"><div class="media-left"><button href="#" class="btn btn-primary">Tweet this&nbsp;<span class="fa fa-twitter"></span></button></div><div class="media-body">'+ text +'</div></div></div>';
+      $(this).html(template);
 
-       var template = '<div class="tweet-intent"><div class="media"><div class="media-left"><button href="#" class="btn btn-primary">Tweet this&nbsp;<span class="fa fa-twitter"></span></button></div><div class="media-body">'+ text +'</div></div></div>';
-       $(this).html(template);
-
-       var tweet = twitterUrl + '?text=' + encodeURIComponent(text);
+      var tweet = twitterUrl + '?text=' + encodeURIComponent(text);
      
-       $(this).find('button').click(function(){
-         window.open(tweet, '_blank', 'height=420, width=550, scrollbars=no');
-       });
+      $(this).find('button').click(function(){
+        window.open(tweet, '_blank', 'height=420, width=550, scrollbars=no');
+      });
+    });
   });
-
-});
-
+}());
